6eebd5bc76615d23b4ff212830985377e326dfa8
natthaorn <natthaorn.l@gmail.com>
2021-10-17 11:57:57 +0700

    EighthPage with StreamBuilder

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:first_app/model/first_form_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'page/eighth_page.dart';
 import 'page/fifth_page.dart';
 import 'page/first_page.dart';
 import 'page/fourth_page.dart';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
             bodyText2: TextStyle(color: Colors.purple),
           ),
         ),
-        initialRoute: '/5',
+        initialRoute: '/8',
         routes: <String, WidgetBuilder>{
           '/1': (context) => FirstPage(),
           '/2': (context) => SecondPage(),
@@ -45,6 +46,7 @@ class MyApp extends StatelessWidget {
           '/5': (context) => FifthPage(),
           '/6': (context) => SixthPage(),
           '/7': (context) => SeventhPage(),
+          '/8': (context) => EighthPage(),
         });
   }
 }
